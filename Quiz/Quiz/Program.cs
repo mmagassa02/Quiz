6b1117188a8e49742d52ae108846a82c3e96f53d
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-22 13:29:27 +0100

    Premier fichier quiz

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int score = 0;
+            string user;
+
+            //Saisie données user
+            Console.WriteLine("Entrez votre nom d'utilisateur");
+            user = Console.ReadLine();
+
+            // Début du quiz
+        }
+
+        
+    }
+}
