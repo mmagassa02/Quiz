17ea982c4ca43673e033822df7a5a17218adc0eb
lagann1 <36117642+lagann1@users.noreply.github.com>
2021-12-23 16:00:57 +0100

    Mise a jour avec ajout de questions dans un fichier.

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -8,33 +8,67 @@ namespace Quiz
     {
         /*     public static void RecuperationFichier(Question question)
              {
-                 string line = null;
-                 using (StreamReader sr = new StreamReader("questions.txt"))
-                 {
-                     while ((line=sr.ReadLine()) != null)
-                     {
-                     }
-                 }
+
              }*/
-        public static void AddQuestions()
+        public static void Init()
         {
-            List<Question> questions = new List<Question>()
+            string saisie;
+            try
             {
-                    new Question("1 Lequel de ces pays est situé en Europe?\nFrance\t\tSingapour\tLiechtenstein", "France Liechtenstein"),
-                    new Question("2 Lequel de ces monuments est situé en Italie?\nColisée\tTour eiffel\tArc de triomphe", "Colisée"),
-                    new Question("3 En quelle année la France a gagné une coupe du monde de football?\n1984\t1998\t2018", "1998 2018"),
-                    new Question("4 Laquelle de ces capitales est située en Asie?\nTripoli\t\tErevan\tSéoul", "Séoul"),
-                    new Question("5 Quelle est la capitale du Canada?\nOttawa\tToronto\tVancouver", "Ottawa"),
-                    new Question("6 Quelle est la capitale de la Nouvelle Zélande\nWellington\tAuckland\tNassau", "Wellington"),
-                    new Question("7 Dans quel pays s'est déroulé la Coupe du Monde 2018?\nBrésil\tQatar\tRussie", "Russie"),
-                    new Question("8 Quelle capitale ne se situe pas dans l'hémisphère nord?\nJakarta\tDoha\tBerlin", "Jakarta"),
-                    new Question("9 Combien d'états possèdent les États-unis?\n50\t51\t52", "50"),
-                    new Question("10 Combien fait 1 x 2 x 3 x 4 x 5 x 6 = ???", "720"),
-            };
-            AskQuestion(questions);
+                Debut:
+                Console.WriteLine("Veuillez saisir un ok pour commencer ou saisissez un code pour ajouter une question");
+                saisie = Console.ReadLine();
+                if(saisie == "ok") 
+                {
+                    List<Question> questions = new List<Question>()
+                    {
+                        new Question("1 Lequel de ces pays est situé en Europe?\nFrance\t\tSingapour\tLiechtenstein", "France Liechtenstein"),
+                        new Question("2 Lequel de ces monuments est situé en Italie?\nColisée\tTour eiffel\tArc de triomphe", "Colisée"),
+                        new Question("3 En quelle année la France a gagné une coupe du monde de football?\n1984\t1998\t2018", "1998 2018"),
+                        new Question("4 Laquelle de ces capitales est située en Asie?\nTripoli\t\tErevan\tSéoul", "Séoul"),
+                        new Question("5 Quelle est la capitale du Canada?\nOttawa\tToronto\tVancouver", "Ottawa"),
+                        new Question("6 Quelle est la capitale de la Nouvelle Zélande\nWellington\tAuckland\tNassau", "Wellington"),
+                        new Question("7 Dans quel pays s'est déroulé la Coupe du Monde 2018?\nBrésil\tQatar\tRussie", "Russie"),
+                        new Question("8 Quelle capitale ne se situe pas dans l'hémisphère nord?\nJakarta\tDoha\tBerlin", "Jakarta"),
+                        new Question("9 Combien d'états possèdent les États-unis?\n50\t51\t52", "50"),
+                        new Question("10 Combien fait 1 x 2 x 3 x 4 x 5 x 6 = ???", "720"),
+                    };
+                }else if(saisie == "mmagassa")
+                {
+                    if (AddQuestionFile() == true)
+                        goto Debut;
+                }
+
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Erreur de saisie");
+            }
+            /*
+            AskQuestion(questions);*/
         }
 
+        public static bool AddQuestionFile()
+        {
+            bool ajout = false;
+            string questionuser, proposition, reponseUser, chaine;
+            Console.WriteLine("Ecrivez dans un premier temps la question...");
+            questionuser = Console.ReadLine();
+            Console.WriteLine("suivie des propositions de réponse...");
+            proposition = Console.ReadLine();
+            Console.WriteLine("et de la ou les réponses possibles (Si plusieurs réponses possibles, les séparer par une virgule");
+            reponseUser = Console.ReadLine();
 
+            chaine = questionuser+"#"+proposition+"#"+reponseUser;
+            StreamWriter sw = new StreamWriter("questions.txt");
+            sw.WriteLine(chaine);
+            sw.Flush();
+            sw.Close();
+            Console.WriteLine("La question a été ajoutée");
+            ajout = true;
+            return ajout;
+        
+        }
 
 
 
@@ -85,7 +119,7 @@ namespace Quiz
 
         static void Main(string[] args)
         {
-            AddQuestions();
+            Init();
 
             // Variables
             /*
